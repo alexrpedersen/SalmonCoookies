9cd2b9010ecfe21e3038d63af24aac7b1cf8efc5
Alex Pedersen <apedersen@Titan.localdomain>
2020-02-11 08:52:15 -0800

    adding function

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am'];
+
+
+
 var seattleStore = {
   name: 'Seattle Store',
   minCust: 23,
   maxCust: 65,
   avgCookie: 6.3,
-  hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+ 
 
 randomCust: function() {
   Math.floor((Math.random() * 65) + 1)
@@ -15,7 +19,7 @@ var tokyoStore = {
   minCust: 3,
   maxCust: 24,
   avgCookie: 1.2,
-  hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+
 
 randomCust: function() {
   Math.floor((Math.random() * 24) + 1)
@@ -27,7 +31,7 @@ var dubaiStore = {
   minCust: 11,
   maxCust: 38,
   avgCookie: 3.7,
-  hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+
 
 randomCust: function() {
   Math.floor((Math.random() * 38) + 1)
@@ -39,7 +43,7 @@ var parisStore = {
   minCust: 20,
   maxCust: 38,
   avgCookie: 2.3,
-  hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+
 
 randomCust: function() {
   Math.floor((Math.random() * 38) + 1)
@@ -51,10 +55,58 @@ var limaStore = {
   minCust: 2,
   maxCust: 16,
   avgCookie: 4.6,
-  hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
-randomCust: function() {
-  this.avgCookie.Math.floor((Math.random() * 16) + 1)
-  }
+
+function getrandomcust(min,max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
+var time = [6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+var testNumber =  getrandomcust(Seattle.mincust, Seattle.maxcust);
+function calculateCookes(){
+  for (var i = 0; i < time.length; i++) {
+    var testNumber =  getrandomcust(Seattle.mincust, Seattle.maxcust);
+
+    var answ = (testNumber * Seattle.avgcookie);
+    console.log(time[i] , " o'clock: " ,Math.floor(answ) , "cookies sold");
+  }
+}
+calculateCookes();
+
+var time = [6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+var testNumber =  getrandomcust(Tokyo.mincust, Tokyo.maxcust);
+function calculateCookes(){
+  for (var i = 0; i < time.length; i++) {
+    var testNumber =  getrandomcust(Tokyo.mincust, Tokyo.maxcust);
+
+    var answ = (testNumber * Tokyo.avgcookie);
+    console.log(time[i] , " o'clock: " ,Math.floor(answ) , "cookies sold");
+  }
+}
+calculateCookes();
+
+var time = [6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+var testNumber =  getrandomcust(Dubai.mincust, Dubai.maxcust);
+function calculateCookes(){
+  for (var i = 0; i < time.length; i++) {
+    var testNumber =  getrandomcust(Dubai.mincust, Dubai.maxcust);
+
+    var answ = (testNumber * Dubai.avgcookie);
+    console.log("",time[i] , " o'clock: " ,Math.floor(answ) , "cookies sold");
+  }
+}
+calculateCookes();
+var time = [6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+var testNumber =  getrandomcust(Lima.mincust, Lima.maxcust);
+function calculateCookes(){
+  for (var i = 0; i < time.length; i++) {
+    var testNumber =  getrandomcust(Lima.mincust, Lima.maxcust);
+
+    var answ = (testNumber * Lima.avgcookie);
+    console.log("Seattle", time[i] , " o'clock: " ,Math.floor(answ) , "cookies sold");
+  }
+}
+calculateCookes();
